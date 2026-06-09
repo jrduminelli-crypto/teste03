64c84115c30ee1e5bf651c642d3d4440d1140231
jrduminelli-crypto <jrduminelli@gmail.com>
2026-06-09 08:37:01 -0300

    Add files via upload
    teste

---
+++ data.js
@@ -0,0 +1,109 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-eyestetix-studio-qpr7fqc6wgm-unsplash",
+      "name": "eyestetix-studio-qPR7fQc6WGM-unsplash",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 1.2247187700619335,
+        "pitch": 0,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6566869892680174,
+          "pitch": 0.010097655566802644,
+          "rotation": 0,
+          "target": "1-eyestetix-studio-zqmmbgbvmju-unsplash"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.2290409924614902,
+          "pitch": 0,
+          "title": "foto 02",
+          "text": "bbbbbb<div>bbbbbb</div>"
+        }
+      ]
+    },
+    {
+      "id": "1-eyestetix-studio-zqmmbgbvmju-unsplash",
+      "name": "eyestetix-studio-ZqMmbgbvmJU-unsplash",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.027291890498906568,
+        "pitch": 0,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.0001853029000464801,
+          "pitch": 0,
+          "rotation": 0,
+          "target": "0-eyestetix-studio-qpr7fqc6wgm-unsplash"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.05911115860011762,
+          "pitch": 0,
+          "title": "foto1",
+          "text": "aaaaaaaa<div>aaaaaa</div>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
